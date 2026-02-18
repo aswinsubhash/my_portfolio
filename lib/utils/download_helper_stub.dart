1a6e6701c994af5139bfeab3b90fe11c9e5473e2
Aswin Subhash <aswinsubhash@Mac.lan>
2026-02-19 02:41:28 +0400

    chore: fix all remaining lint errors

--- lib/utils/download_helper_stub.dart
+++ lib/utils/download_helper_stub.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
+
 class DownloadHelper {
   static void download(String url, String fileName) {
     // No-op for non-web platforms or fallback
-    print('Download not supported on this platform');
+    debugPrint('Download not supported on this platform');
   }
 }
